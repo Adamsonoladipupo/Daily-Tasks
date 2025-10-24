4084ad56d755259a92e666fb5e433a0912e4fb4d
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-24 19:06:39 +0100

    new codes

---
+++ trueLoveFlowerCheck.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class trueLoveFlowerCheck{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		int timmy = input.nextInt();
+		int sarah = input.nextInt();
+
+		int check = timmy + sarah;
+		
+	}
+	public static boolean chectForTrueLook(){
+		
+	}
+}
