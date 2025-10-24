f8936ae4cf8f440f7bc8d9443569b6a748b6daae
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-10-24 06:18:47 +0100

    task 2

--- true_love_flower_check.py
+++ true_love_flower_check.py
@@ -1,4 +1,9 @@
+def true_love_check(male, female):
+	check = male + female
+	check = check % 2 == 1
+	return check 
+
 timmy = int(input("Hey Timmy, pick a number: "))
 sarah = int(input("Hey Sarah, pick a number: "))
-check = timmy + sarah
-print ("True Love: ", (check % 2 == 1))
+love = true_love_check(timmy, sarah)
+print ("True Love: ", (love))
