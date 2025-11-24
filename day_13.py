934386574a34858039580ef6cbaaf1696212d48d
Ola-Plenty <adamsonoladipupo@gmail.com>
2025-11-24 06:57:55 +0100

    day 13 python

---
+++ day_13.py
@@ -0,0 +1,17 @@
+def  find_next_square(number):
+    new_perfect_square = 0
+    if (number ** 0.5) % 1 == 0:
+        while (number+1 ** 0.5) != 0:
+            number += 1
+            if (number ** 0.5) % 1 == 0:
+                new_perfect_square = number
+                break
+        return new_perfect_square
+    else:
+        return -1
+    print("is not perfect") 
+
+
+print("Welcome, this function helps you get the next perfect sqaure after your input")
+user_input = int(input("Enter a number: "))
+print(find_next_square(user_input))
